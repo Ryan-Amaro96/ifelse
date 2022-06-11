da42bce2be8595a1e0a43efa9ba3bb40d9b1e1d7
Ryan Amaro <ryanamaro2003.st@gmail.com>
2022-06-11 00:37:16 -0300

    first commit

---
+++ script.js
@@ -0,0 +1,22 @@
+alert('ola')
+let age = 19
+if(age>18){
+    console.log('você é maior de idade')
+}
+
+let idade = 19
+let human = true
+if(idade>=18 && human=true){
+   console.log('Você é humano maior de idade') 
+}
+
+let monthBirth ='Janeiro'
+let birthMonth ='Dezembro'
+if(monthBirth=true || birthMonth=true){
+    console.log('Parabéns, você faz aniversário no último ou no primeiro mês do ano')
+}
+let name = 'Ryan'
+substring(0)="R"
+if(substring(0)=true){
+    console.log('Seu nome começa com a letra "R"')
+}
